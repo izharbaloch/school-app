9d2d06f9c20d9e4848d3b0aa80b78817e635a64e
unknown <izharbaloch570@gmail.com>
2026-04-13 20:56:54 +0500

    dashboard card add but change in future

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+use App\Models\Teacher;
+use App\Models\Guardian;
+use App\Models\Exam;
+use App\Models\StudentFee;
+use App\Models\FeePayment;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    public function dashboard()
+    {
+        $data = [
+            'totalStudents' => Student::count(),
+            'totalTeachers' => Teacher::count(),
+            'totalGuardians' => Guardian::count(),
+            'totalExams' => Exam::count(),
+            'totalFees' => StudentFee::count(),
+            'feesPaid' => FeePayment::count(),
+        ];
+
+        return view('dashboard', $data);
+    }
+
     public function academicSetupView()
     {
         return view('admin.academic-setup');
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,10 +2,101 @@
 @section('title', 'dashboard')
 @section('content')
     <div class="section-header">
-        <h1>Blank Page</h1>
+        <h1>Dashboard</h1>
     </div>
 
     <div class="section-body">
-        <h1>this is dashboard</h1>
+         <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                  <i class="fas fa-graduation-cap"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Students</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $totalStudents ?? 0 }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                  <i class="fas fa-chalkboard-user"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Teachers</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $totalTeachers ?? 0 }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-warning">
+                  <i class="fas fa-users"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Guardians</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $totalGuardians ?? 0 }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-success">
+                  <i class="fas fa-file-alt"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Exams</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $totalExams ?? 0 }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-info">
+                  <i class="fas fa-receipt"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Total Fees</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $totalFees ?? 0 }}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-secondary">
+                  <i class="fas fa-money-bill-wave"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Fees Paid</h4>
+                  </div>
+                  <div class="card-body">
+                    {{ $feesPaid ?? 0 }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
     </div>
 @endsection
